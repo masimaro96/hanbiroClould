20fc5172001da0a5782b0c43e904eae5cdaf6203
masimaro96 <masimaro1996@gmail.com>
2022-10-20 11:13:29 +0700

    init source

---
+++ themeforest/template/src/nqui/App.jsx
@@ -0,0 +1,16 @@
+import './App.css';
+import React from 'react';
+import Grids from './Grids';
+/* eslint-disable */
+
+function App() {
+  
+  return (
+    <div className='App'>
+    
+    <Grids/>
+    </div>
+  );
+}
+
+export default App;
