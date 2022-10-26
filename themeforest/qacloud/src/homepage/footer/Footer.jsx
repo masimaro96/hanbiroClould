94553212bdec340f014c2c11981036638549e6ec
masimaro96 <masimaro1996@gmail.com>
2022-10-26 16:12:13 +0700

    merge code - detailpage

--- themeforest/qacloud/src/homepage/footer/Footer.jsx
+++ themeforest/qacloud/src/homepage/footer/Footer.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import styled from 'styled-components';
-import '../ngoc/main.css';
+import '../footer/main.css';
 import {
   Container, Row, Col
 } from 'react-bootstrap';
