430e5fa45233d180a883804d4b57fcf6896c9b11
NGOC\Ngoc <ngocmy520999@gmail.com>
2022-10-27 15:40:26 +0700

    fix conflict file index

--- themeforest/qacloud/src/detailspage/index.js
+++ themeforest/qacloud/src/detailspage/index.js
@@ -1,25 +1,55 @@
+import React from 'react';
+import Computebaremetalpage from './Page/compute-bare-metal';
+import Computevps from './Page/compute-vps';
+import Computeautoscale from './Page/compute-auto-scale';
+import Computeloadbalance from './Page/compute-load-balance';
+import Computedatabase from './Page/compute-database';
+import Computems from './Page/compute-ms';
+import Clustercloudelasticsearch from './Page/cluster-cloud-elastic-search';
+import Clusterkafka from './Page/cluster-kafka';
+import Clustercloudrabbitmq from './Page/cluster-cloud-rabbitmq';
+import Clustercloudmysqldb from './Page/cluster-cloud-mysql-db';
+import Clustercloudpostgresqldb from './Page/cluster-cloud-postgresql-db';
+import Clustercloudvitess from './Page/cluster-cloud-vitess';
+import Clustercloudmongodb from './Page/cluster-cloud-mongodb';
+import Clustercloudredis from './Page/cluster-cloud-redis';
+import Clustercloudscylladb from './Page/cluster-cloud-scylladb';
+import Clusterclickhouse from './Page/cluster-clickhouse';
+import Storeobjectstore from './Page/store-object-store';
+import Storeblockstore from './Page/store-block-store';
+import Storenasstore from './Page/store-nas-store';
 
 
-import TopNav from '../homepage/header-banner/TopNav';
-import CloudServices from '../homepage/cloud-services/cloudservices';
-import CloudFeatures from '../homepage/cloud-features/cloud-features';
-import Cloudconsult from '../homepage/consult/cloudconsult';
-import Footer from '../homepage/footer/Footer';
+const Pagedetails = () => {
+    
+    return (
+        <header>
+        <Computevps />
+        <Computebaremetalpage />
+        <Computeautoscale />
+        <Computeloadbalance />
+        <Computedatabase />
 
-function App() {
-  return (
-    <div className="App">
-      <header>
-        <TopNav />
-      </header>
-      <body>
-        test
-      </body>
-      <footer>
-        <Footer />
-      </footer>
-    </div>
-  );
-}
+        <Computems />
+        <Clustercloudelasticsearch />
+        <Clusterkafka />
+        <Clustercloudrabbitmq />
+        <Clustercloudmysqldb />
+        <Clustercloudpostgresqldb />
+        <Clustercloudvitess />
+        <Clustercloudmongodb />
+        <Clustercloudredis />
+        <Clustercloudscylladb />
 
-export default App;
+        <Clusterclickhouse />
+        <Storeobjectstore />
+        <Storeblockstore />
+        <Storenasstore />
+
+        </header>
+        
+
+    );
+};
+
+export default Pagedetails;
