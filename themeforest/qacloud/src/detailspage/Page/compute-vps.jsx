cf33886f371e34fd7fde3aac201a39316a4b875c
masimaro96 <masimaro1996@gmail.com>
2022-10-27 18:06:42 +0700

    merge code - edit page

--- themeforest/qacloud/src/detailspage/Page/compute-vps.jsx
+++ themeforest/qacloud/src/detailspage/Page/compute-vps.jsx
@@ -4,7 +4,7 @@ import Footer from '../../homepage/footer/Footer'
 import Cloudconsult from '../../homepage/consult/cloudconsult';
 import Banner from '../../container/Banner';
 import DetailContent from '../../homepage/cloud-features/detail-content';
-import Cardintroducecontent from '../../container/Card';
+import BoxCard from '../../container/Box';
 import {
     Container, Row, Col
   } from 'react-bootstrap';
@@ -22,8 +22,9 @@ function Computevps(){
                         <Sidebar />
                     </Col>
                     <Col md={9}>
+                    <BoxCard />
                     <DetailContent />
-
+                    
                     </Col>
                 </Row>
             </Container>         
