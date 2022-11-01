5008e9e2e77fa891c13a0c045ac64ff0549f2241
masimaro96 <masimaro1996@gmail.com>
2022-11-01 14:25:27 +0700

    merge code - add link to navbar

--- themeforest/qacloud/src/homepage/header-banner/TopNavButton.jsx
+++ themeforest/qacloud/src/homepage/header-banner/TopNavButton.jsx
@@ -56,11 +56,11 @@ function titleMenu($Kor,$Eng){
         </>
     )
 }
-function titlePop($data){
+function titlePop($link,$data){
     return(
         <>
            
-            <div > <AiOutlineRight className="icon-right"/>{$data}</div>
+            <div ><a href={$link}> <AiOutlineRight className="icon-right"/>{$data}</a></div>
             
         </>
     )
@@ -245,21 +245,21 @@ function NavButton() {
                         {isShownGw && (
                         <div className="groupware">
                             
-                            {titlePop (" 가격표")}
-                            {titlePop (" 신청하기")}
-                            {titlePop (" 한비로 그룹웨어에 대해")}
-                            {titlePop (" 구성 메뉴와 특징")}
-                            {titlePop (" 메일")}
-                            {titlePop (" HR")}
-                            {titlePop (" 메신저")}
-                            {titlePop (" CloudDisk")}
-                            {titlePop (" CRM")}
-                            {titlePop (" 모바일앱")}
-                            {titlePop (" 동영상 도움말")}
-                            {titlePop (" 외부 백업")}
-                            {titlePop (" 스크린샷")}
-                            {titlePop (" 서비스 안내")}
-                            {titlePop (" 문의하기")}
+                            {titlePop ("https://hanbiro.com/software/groupware-price.html"," 가격표")}
+                            {titlePop ("https://hanbiro.com/software/groupware-free-trial.html?SM=SPCMAKO"," 신청하기")}
+                            {titlePop ("https://hanbiro.com/software/overview-video.html"," 한비로 그룹웨어에 대해")}
+                            {titlePop ("https://hanbiro.com/software/business-email-software.html"," 구성 메뉴와 특징")}
+                            {titlePop ("https://hanbiro.com/software/big-data-email-features.html"," 메일")}
+                            {titlePop ("https://hanbiro.com/software/hanbiro-hr-52-hour-work-week.html"," HR")}
+                            {titlePop ("https://hanbiro.com/software/messenger-outline.html"," 메신저")}
+                            {titlePop ("https://hanbiro.com/software/cloud-disk-outline.html"," CloudDisk")}
+                            {titlePop ("https://hanbiro.com/software/about-crm.html"," CRM")}
+                            {titlePop ("https://hanbiro.com/software/groupware-features.html"," 모바일앱")}
+                            {titlePop ("https://hanbiro.com/software/manual-video-groupware.html"," 동영상 도움말")}
+                            {titlePop ("https://hanbiro.com/software/backup.html"," 외부 백업")}
+                            {titlePop ("https://hanbiro.com/software/hanbiro-groupware-screenshots.html"," 스크린샷")}
+                            {titlePop ("https://hanbiro.com/software/service-type-guide.html"," 서비스 안내")}
+                            {titlePop ("https://hanbiro.com/software/send-a-message.html"," 문의하기")}
                                
 
                         </div>
@@ -267,11 +267,11 @@ function NavButton() {
                         {isShownVora && (
                         <div className="vora">
                             
-                            {titlePop ("Vora Email")}
-                            {titlePop ("Vora HR")}
-                            {titlePop ("Vora Project")}
-                            {titlePop ("Vora Call")}
-                            {titlePop ("Vora CloudDisk")}
+                            {titlePop ("https://hanbiro.com/software/vora-email-features.html","Vora Email")}
+                            {titlePop ("https://hanbiro.com/software/vora-hr-52-hour-work-week.html","Vora HR")}
+                            {titlePop ("https://hanbiro.com/software/vora-project-new.html","Vora Project")}
+                            {titlePop ("https://hanbiro.com/software/vora-call-summary.html","Vora Call")}
+                            {titlePop ("https://hanbiro.com/software/vora-cloud-disk-outline.html","Vora CloudDisk")}
                                
 
                         </div>
@@ -283,23 +283,23 @@ function NavButton() {
                                     <div className="product-list">
                                         <div className="menu-list-item boder-left">
                                             <h6 > 한비로</h6>
-                                            {titlePop ("소개")}
-                                            {titlePop ("CEO 인사말")}
-                                            {titlePop ("Brand")}
-                                            {titlePop ("연혁")}
-                                            {titlePop ("한비로 사람들")}
-                                            {titlePop ("회원증 및 인증서")}
-                                            {titlePop ("오시는 길")}
-                                            {titlePop ("블로그")}
+                                            {titlePop ("https://hanbiro.com/hanbiro/about.html","소개")}
+                                            {titlePop ("https://hanbiro.com/hanbiro/ceo.html","CEO 인사말")}
+                                            {titlePop ("https://hanbiro.com/hanbiro/brand.html","Brand")}
+                                            {titlePop ("https://hanbiro.com/hanbiro/history.html","연혁")}
+                                            {titlePop ("https://hanbiro.com/hanbiro/organization.html","한비로 사람들")}
+                                            {titlePop ("https://hanbiro.com/hanbiro/authentication.html","회원증 및 인증서")}
+                                            {titlePop ("https://hanbiro.com/hanbiro/contact-us.html","오시는 길")}
+                                            {titlePop ("https://blog.naver.com/hanbiro1999","블로그")}
                                         </div>
                                         <div className="menu-list-item">
                                             <h6>  고객센터</h6>
-                                            {titlePop ("고객 문의 및 서버 점검 의뢰")}
-                                            {titlePop ("결제 안내")}
-                                            {titlePop ("IDC 방문 신청")}
-                                            {titlePop ("불편 사항 접수")}
-                                            {titlePop ("네임 서버 정보")}
-                                            {titlePop ("메일 POP / 모바일 POP 설정")}
+                                            {titlePop ("https://hanbiro.com/customer/inquiry.html","고객 문의 및 서버 점검 의뢰")}
+                                            {titlePop ("https://hanbiro.com/customer/bank.html","결제 안내")}
+                                            {titlePop ("https://hanbiro.com/customer/idc-visit.html","IDC 방문 신청")}
+                                            {titlePop ("https://hanbiro.com/customer/complain.html","불편 사항 접수")}
+                                            {titlePop ("https://hanbiro.com/customer/name-server.html","네임 서버 정보")}
+                                            {titlePop ("https://hanbiro.com/customer/set-pop-livemail.html","메일 POP / 모바일 POP 설정")}
                                         </div>
                                     </div>
                                 </div>
@@ -311,88 +311,88 @@ function NavButton() {
                                     <div className="product-list">
                                         <div className="menu-list-item boder-left">
                                             <h6>관리 운영 서버 호스팅이란?</h6>
-                                            {titlePop ("소개")}
-                                            {titlePop ("필요성")}
-                                            {titlePop ("관리 내역")}
-                                            {titlePop ("서버 호스팅과의 차이점")}
+                                            {titlePop ("https://hanbiro.com/management/management-outline.html","소개")}
+                                            {titlePop ("https://hanbiro.com/management/management-necessity.html","필요성")}
+                                            {titlePop ("https://hanbiro.com/management/management-details.html","관리 내역")}
+                                            {titlePop ("https://hanbiro.com/management/management-difference.html","서버 호스팅과의 차이점")}
                                             
                                         </div>
                                         <div className="menu-list-item boder-left">
                                             <h6>관리 운영 서버 서비스 종류와 가격</h6>
-                                            {titlePop ("상품 구성")}
-                                            {titlePop ("관리형 코로케이션")}
-                                            {titlePop ("단기 임대 서버 호스팅")}
-                                            {titlePop ("원격 서버 관리 운영")}
-                                            {titlePop ("클라우드 서비스와의 가격 비교")}
-                                            {titlePop ("신청하기")}
+                                            {titlePop ("https://hanbiro.com/management/product-list.html","상품 구성")}
+                                            {titlePop ("https://hanbiro.com/management/customer.html","관리형 코로케이션")}
+                                            {titlePop ("https://hanbiro.com/management/event-manage.html","단기 임대 서버 호스팅")}
+                                            {titlePop ("https://hanbiro.com/management/remote-manage.html","원격 서버 관리 운영")}
+                                            {titlePop ("https://hanbiro.com/management/price-comparison.html","클라우드 서비스와의 가격 비교")}
+                                            {titlePop ("https://hanbiro.com/application/index.html?Manage=mt1","신청하기")}
 
                                         </div>
                                         <div className="menu-list-item boder-left">
                                             <h6> 서버 호스팅</h6>
-                                            {titlePop ("상품 구성")}
-                                            {titlePop ("코로케이션")}
-                                            {titlePop ("단기 임대 서버 호스팅")}
-                                            {titlePop ("클라우드 서비스와의 가격 비교")}
-                                            {titlePop ("유료 기술 지원")}
-                                            {titlePop ("신청하기")}
+                                            {titlePop ("https://hanbiro.com/hosting/product-list.html","상품 구성")}
+                                            {titlePop ("https://hanbiro.com/hosting/customer.html","코로케이션")}
+                                            {titlePop ("https://hanbiro.com/hosting/event-manage.html","단기 임대 서버 호스팅")}
+                                            {titlePop ("https://hanbiro.com/hosting/price-comparison.html","클라우드 서비스와의 가격 비교")}
+                                            {titlePop ("https://hanbiro.com/hosting/serverhosting-support-outline.html","유료 기술 지원")}
+                                            {titlePop ("https://hanbiro.com/application/index.html?Manage=mt2","신청하기")}
                                         </div>
                                         <div className="menu-list-item boder-left">
                                             <h6>웹 호스팅</h6>
-                                            {titlePop ("리눅스")}
-                                            {titlePop ("윈도우")}
-                                            {titlePop ("DRM 스트리밍 호스팅")}
-                                            {titlePop ("부가 서비스")}
-                                            {titlePop ("유료 기술 지원")}
-                                            {titlePop ("신청하러 가기")}
+                                            {titlePop ("https://hanbiro.com/hosting/webhosting-linux.html","리눅스")}
+                                            {titlePop ("https://hanbiro.com/hosting/webhosting-windows.html","윈도우")}
+                                            {titlePop ("https://hanbiro.com/hosting/webhosting-drm.html","DRM 스트리밍 호스팅")}
+                                            {titlePop ("https://hanbiro.com/hosting/webhosting-optional-service.html","부가 서비스")}
+                                            {titlePop ("https://hanbiro.com/hosting/webhosting-support1.html","유료 기술 지원")}
+                                            {titlePop ("https://mypage.hanbiro.com/","신청하러 가기")}
 
                                         </div>
                                         <div className="menu-list-item ">
                                             <h6>베트남 호스팅</h6>
-                                            {titlePop ("상품 구성")}
-                                            {titlePop ("IDC 소개")}
-                                            {titlePop ("네트워크 구성")}
-                                            {titlePop ("신청하기")}
-                                            {titlePop ("베트남 사무실")}
+                                            {titlePop ("https://hanbiro.com/overseas/vietnam-product-list.html","상품 구성")}
+                                            {titlePop ("https://hanbiro.com/overseas/vietnam-idc.html","IDC 소개")}
+                                            {titlePop ("https://hanbiro.com/overseas/vietnam-network.html","네트워크 구성")}
+                                            {titlePop ("https://hanbiro.com/application/index.html?Manage=mt4&Country=vt","신청하기")}
+                                            {titlePop ("https://hanbiro.com/overseas/vietnam-office.html","베트남 사무실")}
                                             
                                         </div>
                                     </div>
                                     <div className="product-list">
                                         <div className="menu-list-item boder-left">
                                             <h6>DDos</h6>
-                                            {titlePop ("한비로 IPS 소개")}
-                                            {titlePop ("방어 능력")}
-                                            {titlePop ("공격 유형과 차단")}
-                                            {titlePop ("고객 관리자 페이지")}
+                                            {titlePop ("https://hanbiro.com/hosting/ddos-outline.html","한비로 IPS 소개")}
+                                            {titlePop ("https://hanbiro.com/hosting/ddos-defend.html","방어 능력")}
+                                            {titlePop ("https://hanbiro.com/hosting/ddos-attack-type.html","공격 유형과 차단")}
+                                            {titlePop ("https://hanbiro.com/hosting/ddos-admin.html","고객 관리자 페이지")}
                                             
                                         </div>
                                         <div className="menu-list-item boder-left">
                                             <h6>네트워크 구성도 및 트래픽</h6>
-                                            {titlePop ("네트워크 구성도")}
-                                            {titlePop ("네트워크와 트래픽")}
+                                            {titlePop ("https://hanbiro.com/management/network-component.html","네트워크 구성도")}
+                                            {titlePop ("https://hanbiro.com/management/network-traffic.html","네트워크와 트래픽")}
 
                                         </div>
                                         <div className="menu-list-item boder-left">
                                             <h6>부가 서비스</h6>
-                                            {titlePop ("MS-ASP 라이선스")}
-                                            {titlePop ("서버 구축 및 운영")}
-                                            {titlePop ("서버 관제 서비스")}
-                                            {titlePop ("보안 서비스")}
-                                            {titlePop ("네트워크")}
-                                            {titlePop ("소프트웨어")}
-                                            {titlePop ("기타")}
-                                            {titlePop ("무료 부가 서비스")}
+                                            {titlePop ("https://hanbiro.com/management/ms.html","MS-ASP 라이선스")}
+                                            {titlePop ("https://hanbiro.com/management/backup.html","서버 구축 및 운영")}
+                                            {titlePop ("https://hanbiro.com/management/sys-monitor.html","서버 관제 서비스")}
+                                            {titlePop ("https://hanbiro.com/management/security.html","보안 서비스")}
+                                            {titlePop ("https://hanbiro.com/management/vpn.html","네트워크")}
+                                            {titlePop ("https://hanbiro.com/management/groupware.html","소프트웨어")}
+                                            {titlePop ("https://en.hanbiro.com/","기타")}
+                                            {titlePop ("https://hanbiro.com/management/free-optional-services.html","무료 부가 서비스")}
                                         </div>
                                         <div className="menu-list-item boder-left">
                                             <h6>기술 인프라</h6>
-                                            {titlePop ("로드밸런싱 / 클러스터링")}
-                                            {titlePop ("DB 클러스터링")}
-                                            {titlePop ("데이터 이중화 서비스")}
-                                            {titlePop ("스토리지 시스템")}
-                                            {titlePop ("Windows DRM")}
-                                            {titlePop ("웹 캐싱 서비스")}
-                                            {titlePop ("DB 암호화 시스템 구축")}
-                                            {titlePop ("DR(재해 복구) 시스템 구축")}
-                                            {titlePop ("IPMI 서비스")}
+                                            {titlePop ("https://hanbiro.com/management/clustering-type.html","로드밸런싱 / 클러스터링")}
+                                            {titlePop ("https://hanbiro.com/management/mysql-cluster.html","DB 클러스터링")}
+                                            {titlePop ("https://hanbiro.com/management/data-dual-processor.html","데이터 이중화 서비스")}
+                                            {titlePop ("https://hanbiro.com/management/iscsi-storage.html","스토리지 시스템")}
+                                            {titlePop ("https://hanbiro.com/management/drm.html","Windows DRM")}
+                                            {titlePop ("https://hanbiro.com/management/webcaching.html","웹 캐싱 서비스")}
+                                            {titlePop ("https://hanbiro.com/management/db-secure.html","DB 암호화 시스템 구축")}
+                                            {titlePop ("https://hanbiro.com/management/recovery.html","DR(재해 복구) 시스템 구축")}
+                                            {titlePop ("https://hanbiro.com/management/recovery.html","IPMI 서비스")}
 
                                         </div>
                                         <div className="menu-list-item ">
