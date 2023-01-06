ab45a8e05acab3d6c4151fd29651503c4cb941f2
NGOC\Ngoc <ngocmy520999@gmail.com>
2023-01-06 10:08:28 +0700

    edit footer

--- themeforest/qacloud/src/homepage/footer/Footer.jsx
+++ themeforest/qacloud/src/homepage/footer/Footer.jsx
@@ -16,6 +16,7 @@ import EmailIcon from 'mdi-react/EmailIcon';
 import PhoneIcon from 'mdi-react/PhoneIcon';
 import FaxIcon from 'mdi-react/FaxIcon';
 import ExternalLinkIcon from 'mdi-react/ExternalLinkIcon';
+import { Box } from '@mui/material';
 function Footer() {
 
   return (
@@ -23,10 +24,10 @@ function Footer() {
         <div className='footer-top'>
             <Container className='footer-body'>
                 <Row>
-                    <Col md={9} className='logo-footer'>
-                        <Card>
-                            <CardBody className='logo-footer-detail'>
-                                <img src='https://www.hanbiro.com/images/logo-footer.png' />
+                    <Col md={12} lg={9} className='logo-footer'>
+                        <Box>
+                            <Box className='logo-footer-detail'>
+                                <img className='logo-footer-img' src='https://www.hanbiro.com/images/logo-footer.png' />
                                 <p className="typography-message">
                                     · 본사 : 도로명 주소 - 서울시 서초구 남부순환로333길 9(서초동, 효천빌딩) 4,5층 | 지번 주소 - 서울특별시 서초구 서초1동 1425-10 효천빌딩 4,5층
                                 <br />
@@ -40,27 +41,27 @@ function Footer() {
                                 <br />
                                 · 서버 운영실 : 서울 서초구 서초동 1423-1 KIDC 서초1센타 2층
                                 <br />
+                                <EmailIcon className='icon-text'/> <a href='mailto:hanbiro@hanbiro.com' className='text-color'>hanbiro@hanbiro.com</a>  &nbsp;&nbsp;&nbsp;<PhoneIcon className='icon-text'/>Phone: <a href='tel:1544-4755' className='text-color'>1544-4755</a>  &nbsp;&nbsp;&nbsp;<FaxIcon className='icon-text'/>Fax: 02) 6919-2050
                                 </p>
-                                <p><EmailIcon className='icon-text'/> <span className='text-color'>hanbiro@hanbiro.com</span>  &nbsp;&nbsp;&nbsp;<PhoneIcon className='icon-text'/>Phone: <span className='text-color'>1544-4755</span>  &nbsp;&nbsp;&nbsp;<FaxIcon className='icon-text'/>Fax: 02) 6919-2050</p>
-                                <p>법인명 : (주)한비로 | 대표 : 권택술 | 사업자 등록 번호 : 220-87-04858 | 통신 판매업 신고 번호 : 서초 제09110<span className='text-color'>호 사업자 정보 확인 <ExternalLinkIcon className='icon-text'/></span>  | <br /> 개인 정보 관리 책임 : 운영 정보팀 부장 이용덕</p>
-                            </CardBody>
-                        </Card>
+                                <p>법인명 : (주)한비로 | 대표 : 권택술 | 사업자 등록 번호 : 220-87-04858 | 통신 판매업 신고 번호 : 서초 제09110호<a href='http://ftc.go.kr/www/bizCommView.do?key=232&apv_perm_no=2006321007630209110&pageUnit=10&searchCnd=bup_nm&searchKrwd=%ED%95%9C%EB%B9%84%EB%A1%9C&pageIndex=1' className='text-color external-link'> 사업자 정보 확인 <ExternalLinkIcon className='icon-text icon-external-link'/></a>  |  개인 정보 관리 책임 : 운영 정보팀 부장 이용덕</p>
+                            </Box>
+                        </Box>
                     </Col>
 
-                    <Col md={3} className='spaced'>
-                        <Card>
-                            <CardBody>
-                            <h4>Explore</h4>
-                            <List>
-                                <li><a href='https://hanbiro.com/cloud/'><ChevronRightIcon /> 클라우드</a></li>
-                                <li><a href='https://hanbiro.com/software/overview-video.html'><ChevronRightIcon /> 그룹웨어</a></li>
-                                <li><a href='https://hanbiro.com/software/vora-email-features.html'><ChevronRightIcon /> Vora Works</a></li>
-                                <li><a href='https://hanbiro.com/management/management-outline.html'><ChevronRightIcon /> 호스팅</a></li>
-                                <li><a href='https://hanbiro.com/hanbiro/about.html'><ChevronRightIcon /> 한비로</a></li>
-                                <li><a href='https://hanbiro.com/customer/inquiry.html'><ChevronRightIcon /> 고객 센터</a></li>
-                            </List>
-                            </CardBody>
-                        </Card>
+                    <Col md={12} lg={3} className='spaced'>
+                        <Box>
+                            <Box>
+                              <h5>Explore</h5>
+                              <List>
+                                  <li><a href='https://hanbiro.com/cloud/'><ChevronRightIcon className='right-icon'/> 클라우드</a></li>
+                                  <li><a href='https://hanbiro.com/software/overview-video.html'><ChevronRightIcon className='right-icon' /> 그룹웨어</a></li>
+                                  <li><a href='https://hanbiro.com/software/vora-email-features.html'><ChevronRightIcon className='right-icon' /> Vora Works</a></li>
+                                  <li><a href='https://hanbiro.com/management/management-outline.html'><ChevronRightIcon className='right-icon' /> 호스팅</a></li>
+                                  <li><a href='https://hanbiro.com/hanbiro/about.html'><ChevronRightIcon className='right-icon' /> 한비로</a></li>
+                                  <li><a href='https://hanbiro.com/customer/inquiry.html'><ChevronRightIcon className='right-icon' /> 고객 센터</a></li>
+                              </List>
+                            </Box>
+                        </Box>
                     </Col>
                 </Row>
             </Container>
@@ -69,7 +70,7 @@ function Footer() {
         <div>
             <Row className='footer-bottom'>
                 <Col md={12}>
-                    <p>All Right Reserved © Hanbiro, Inc.   <span className='text-color'>한비로 소개</span> • <span className='text-color'>오시는 길</span> • <span className='text-color'>개인 정보 처리 방침</span> • <span className='text-color'>서비스 이용 약관</span> • <span className='text-color'>클라우드 서비스 이용 약관</span> • <span className='text-color'>사이트맵</span></p>
+                    <p>All Right Reserved © Hanbiro, Inc. &nbsp;&nbsp;  <a href='https://www.hanbiro.com/hanbiro/about.html' className='text-color external-link'>한비로 소개</a> • <a href='https://www.hanbiro.com/hanbiro/contact-us.html' className='text-color external-link'>오시는 길</a> • <a href='https://www.hanbiro.com/privacy.html' className='text-color external-link'>개인 정보 처리 방침</a> • <a href='https://www.hanbiro.com/terms.html' className='text-color external-link'>서비스 이용 약관</a> • <a href='https://www.hanbiro.com/files/cloudTerms.pdf' className='text-color external-link'>클라우드 서비스 이용 약관</a> • <a href='https://www.hanbiro.com/sitemap.html' className='text-color external-link'>사이트맵</a></p>
                 </Col>
             </Row>
         </div>
